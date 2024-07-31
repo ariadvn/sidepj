34f93d64814862c338c0773597565ba75c64de98
ariadvn <$aria.davani1384@gmail.com>
2024-07-31 17:17:26 +0330

    Update

---
+++ auto_clicker.py
@@ -0,0 +1,16 @@
+#%%
+import pyautogui as ag
+import keyboard as k
+import datetime as dt
+
+k.wait('s')
+while not k.is_pressed('d'):
+    delay = 0.0000000000000001
+    ag.leftClick()
+    ag.PAUSE = delay
+
+
+
+
+
+# %%
